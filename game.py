948ce7af8695769a7aef8379dbeb55ebd77575ca
Rick-Bossen <rickroelof@home.nl>
2020-06-05 23:28:00 +0200

    Fixed bugs preventing game routes from working

--- game.py
+++ game.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, make_response, request, jsonify
 from flask_jwt_extended import jwt_required, get_jwt_identity
 
+from flask_pymongo import ObjectId
 from database import mongo
 
 game_bp = Blueprint("game", __name__)
@@ -13,15 +14,15 @@ def start_game():
     if not request.is_json:
         return make_response(jsonify({"message": "Missing JSON in request"}), 400)
 
-    join_code = request.get_json.get("join_code")
-    rounds = request.get_json.get("rounds")
+    join_code = request.get_json().get("join_code")
+    rounds = request.get_json().get("rounds")
 
     if not join_code:
         return make_response(jsonify({"message": "Missing join_code in request"}), 400)
     if not rounds:
         return make_response(jsonify({"message": "Missing rounds in request"}), 400)
 
-    room = mongo.db.rooms.get_one({"_id": join_code})
+    room = mongo.db.rooms.find_one({"_id": join_code})
 
     if not room:
         return make_response(jsonify({"message": "Room not found"}), 404)
@@ -42,14 +43,14 @@ def start_game():
 
     match_id = mongo.db.games.insert_one(game)
     mongo.db.rooms.delete_one({"_id": join_code})
-    return make_response(jsonify({"message": "Game started", "match_id": match_id}), 200)
+    return make_response(jsonify({"message": "Game started", "match_id": str(match_id.inserted_id)}), 200)
 
 
 @game_bp.route("/<string:match_id>/status", methods=["GET"])
 @jwt_required
 def get_game_status(match_id):
 
-    game = mongo.db.games.find_one({"_id": match_id})
+    game = mongo.db.games.find_one({"_id": ObjectId(match_id)})
 
     if not game:
         return make_response(jsonify({"message": "Match not found"}), 404)
@@ -58,5 +59,7 @@ def get_game_status(match_id):
 
     if not identity["_id"] in game.get("users"):
         return make_response(jsonify({"message": "User not in game"}), 403)
+
+    game["_id"] = str(game["_id"])
 
     return make_response(game, 200)
