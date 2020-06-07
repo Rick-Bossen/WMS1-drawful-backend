a797baeff31ecb50a4acdb0c553a78563b383e14
MartijnVisser98 <m.visser.2@hotmail.com>
2020-06-07 19:17:39 +0200

    Added game/pending for users waiting in the lobby

--- game.py
+++ game.py
@@ -4,6 +4,8 @@ from flask_jwt_extended import jwt_required, get_jwt_identity
 from flask_pymongo import ObjectId
 from database import mongo
 
+from time import sleep
+
 game_bp = Blueprint("game", __name__)
 
 
@@ -33,6 +35,7 @@ def start_game():
         return make_response(jsonify({"message": "Unauthorized"}), 403)
 
     game = {
+        "join_code": join_code,
         "creator_id": room.get("creator_id"),
         "users": room.get("users"),
         "user_drawing": room.get("users")[0],
@@ -44,6 +47,39 @@ def start_game():
     match_id = mongo.db.games.insert_one(game)
     mongo.db.rooms.delete_one({"_id": join_code})
     return make_response(jsonify({"message": "Game started", "match_id": str(match_id.inserted_id)}), 200)
+
+
+@game_bp.route("/pending", methods=["POST"])
+@jwt_required
+def pending_game():
+    if not request.is_json:
+        return make_response(jsonify({"message": "Missing JSON in request"}), 400)
+
+    join_code = request.get_json().get("join_code")
+
+    if not join_code:
+        return make_response(jsonify({"message": "Missing join_code in request"}), 400)
+
+    room = mongo.db.rooms.find_one({"_id": join_code})
+
+    if not room:
+        game = mongo.db.games.find_one({"join_code": join_code})
+        if not game:
+            return make_response(jsonify({"message": "Room not found"}), 404)
+        else:
+            return make_response(jsonify({"message": "Game started", "match_id": str(game['_id'])}), 200)
+
+    identity = get_jwt_identity()
+
+    if identity["_id"] not in room.get("users"):
+        return make_response(jsonify({"message": "Unauthorized"}), 403)
+
+    game = mongo.db.games.find_one({"join_code": join_code})
+    while not game:
+        sleep(1)
+        game = mongo.db.games.find_one({"join_code": join_code})
+
+    return make_response(jsonify({"message": "Game started", "match_id": str(game['_id'])}), 200)
 
 
 @game_bp.route("/<string:match_id>/status", methods=["GET"])
