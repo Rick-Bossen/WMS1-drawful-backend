2ab8ddc1de64380e614068ba6cfc419a0ba831b1
Rick-Bossen <rickroelof@home.nl>
2020-06-10 19:55:19 +0200

    Fixed word_parser.py errors with special characters

--- game_logic/word_parser.py
+++ game_logic/word_parser.py
@@ -1,6 +1,6 @@
 import pathlib
 
-file = open(str(pathlib.Path(__file__).parent) + '/../word_list.txt', "r")
+file = open(str(pathlib.Path(__file__).parent) + '/../word_list.txt', "r", encoding="utf-8")
 words = []
 for line in file:
     words.append(line.strip(",\n"))
