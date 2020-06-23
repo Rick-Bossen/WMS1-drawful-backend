4177bae07e382fa10e7faa6f819fa064e11291cf
MartijnVisser98 <m.visser.2@hotmail.com>
2020-06-23 17:24:23 +0200

    Added dotenv configuration

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from flask import Flask, make_response, jsonify
 from flask_cors import CORS
+
+from dotenv import load_dotenv
+import os
+
 from helpers.database import mongo
 from helpers.jwt_global import jwt
 
@@ -7,20 +11,22 @@ from routes import game_bp
 from routes import room_bp
 from routes import user_bp
 
+# Load config
+load_dotenv()
+
 # Create app
 app = Flask(__name__)
-app.config["DEBUG"] = True
+app.config["DEBUG"] = os.environ.get("DEBUG", True)
 
 # Init CORS
 CORS(app)
 
 # Init database connection
-app.config['MONGO_URI'] = 'mongodb://127.0.0.1:27017/drawful'
+app.config['MONGO_URI'] = os.environ.get("MONGO_URI", "mongodb://127.0.0.1:27017/drawful")
 mongo.init_app(app)
 
 # Init jwt
-# TODO generate random key
-app.config['JWT_SECRET_KEY'] = 'this-is-a-random-key-2323f402vf3h49jh3vb4hrtjk34h5lw34n5vw83l4locmwe4nmtcsek4,5vuy5'
+app.config['JWT_SECRET_KEY'] = os.environ.get("JWT_SECRET", "this-is-a-random-key-2323f402vf3h49jh3vb4hrtjk34h5lw34n5vw83l4locmwe4nmtcsek4,5vuy5")
 jwt.init_app(app)
 
 
